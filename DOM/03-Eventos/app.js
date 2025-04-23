c5fd391a4691a6d166e8675a5c15484888be42b3
Brayan <brayanfernandezjimenez18@gmail.com>
2025-04-23 18:31:30 -0500

    Validacion de los campos del formulario

--- DOM/03-Eventos/app.js
+++ DOM/03-Eventos/app.js
@@ -11,12 +11,55 @@ const contrasena = document.querySelector('[name="Contrasena"]')
 
 //Funciones
 const validar=(event) => {
-  event.preventDefault();
   if (nombre.value == "") {
     alert("llene ese nombre");
     nombre.focus();
+    event.preventDefault();
+  }else if(apellido.value==""){
+    alert("llene su apellido");
+    apellido.focus();
+    event.preventDefault();
+  }else if(telefono.value==""){
+    alert("por favor llene el campo del telefono");
+    telefono.focus();
+    event.preventDefault();
+  }else if(documento.value==""){
+    alert("Por favor ingrese su documento");
+    documento.focus();
+    event.preventDefault();
+  }else if(usuario.value==""){
+    alert("Por favor ingrese su usuario");
+    usuario.focus();
+    event.preventDefault();
+  }else if(contrasena.value==""){
+    alert("Por favor ingrese su contraseña");
+    contrasena.focus();
+    event.preventDefault();
+  }
+}
+
+const validar_letras=(event)=>{
+  let tecla=event.key;
+  const letras=/[a-zñáéíóú]/i;
+  if(!letras.test(tecla)){
+    event.preventDefault();
+  }
+}
+
+const validar_numeros=(event)=>{
+  let tecla=event.key;
+  const numeros=/[0-9]/; 
+  // alert (tecla);
+
+  if(!numeros.test(tecla) && tecla!="Backspace"){
+    event.preventDefault();
   }
 }
 
 //Eventos
 formulario.addEventListener('submit',validar);
+nombre.addEventListener('keydown',validar_letras);
+apellido.addEventListener('keydown',validar_letras);
+telefono.addEventListener('keydown',validar_numeros);
+documento.addEventListener('keydown',validar_numeros);
+
